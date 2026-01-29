1e26e9213a4b82ae0433ed92b2fe3eff193c1cb7
mshaheeru <mshaheeruddin19757@gmail.com>
2026-01-29 12:24:09 +0500

    Fix TenantsModule DI by providing SupabaseConfig

--- backend/src/modules/tenants/tenants.module.ts
+++ backend/src/modules/tenants/tenants.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SupabaseConfig } from '../../common/config/supabase.config';
 import { TenantsController } from './tenants.controller';
 import { TenantsService } from './tenants.service';
 
 @Module({
   controllers: [TenantsController],
-  providers: [TenantsService],
+  providers: [TenantsService, SupabaseConfig],
 })
 export class TenantsModule {}
 
